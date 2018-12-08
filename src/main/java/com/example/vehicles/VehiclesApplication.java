e2047fcee64c16f5a633d65b3599cc0c275343e6
Andrey Semenyuk <andrey@somewhere>
2018-12-08 17:02:14 -0500

    Generating and storing vehicles

--- src/main/java/com/example/vehicles/VehiclesApplication.java
+++ src/main/java/com/example/vehicles/VehiclesApplication.java
@@ -1,12 +1,29 @@
 package com.example.vehicles;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VehiclesApplication {
+	private static final Logger log = LoggerFactory.getLogger(VehiclesApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(VehiclesApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner task(VehiclesRepo repo){
+		return args -> {
+			repo.save(new Vehicle(VehicleType.Car));
+
+			log.info("Database:");
+			for(Vehicle vehicle: repo.findAll()){
+				log.info(vehicle.toString());
+			}
+		};
 	}
 }
